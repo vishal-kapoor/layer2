6ca1cf854f26f6e67262f4e4211f794c40778b15
Vishal Kapoor <vishal.kapoor83@gmail.com>
2021-01-01 13:23:58 +0530

    Further addition to layer2 functionality

--- layer2.h
+++ layer2.h
@@ -10,6 +10,27 @@
 #include <stdio.h>
 #include <string.h>
 
+#define INF_MAX_VLAN 20
+typedef enum {
+                ACCESS,
+                TRUNK,
+                L2_MODE_UNKNOWN
+}l2_mode_t;
+
+/* interface properties */
+typedef struct _inf_prop {
+                          mac_addr_t mac_addr;
+                          l2_mode_t mode;
+                          uint16_t vlan[INF_MAX_VLAN];
+                          /* L3 properties */
+                          uint8_t ip_configured;
+                          ip_addr_t ip_addr;
+                          uint8_t mask; /* 0 - 255 */
+} inf_prop_t;
+
+#define IF_MAC(intf_ptr)   ((intf_ptr)->intf_prop.mac_addr.mac)
+#define IF_IP(intf_ptr)    ((intf_ptr)->intf_prop.ip_add.ip_addr)
+
 struct mac_addr {
 		char mac[6];
 		} __attribute__((packed));;
@@ -40,9 +61,9 @@ typedef struct vlan_hdr vlan_hdr_t;
 struct eth_vlan_hdr {
                     mac_addr_t  dest_mac;
                     mac_addr_t  src_mac;
-                    vlan_hdr_t  vlan;
+                    vlan_hdr_t  vlan_hdr;
                     uint16_t    type;
-                    char        data[256];
+                    char        data[256]; /*Maximum allowed 1500 bytes */
                     uint32_t    fcs;
 } __attribute__((packed));;
 
